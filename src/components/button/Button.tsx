96419c02f83f940796d7edb608b5fbd26a3c6b7f
Sangbin Lee <leesangbin@outlook.com>
2022-03-19 22:20:50 +0100

    initial commit

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,13 @@
+export interface ButtonProps {
+    label?: string;
+}
+
+const Button = ({ label }: ButtonProps) => {
+    return (
+        <button>
+            {label}
+        </button>
+    )
+}
+
+export default Button;
