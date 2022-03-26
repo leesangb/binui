04c55a622d07fce82efef5f549f2d20888dad801
Sangbin Lee <leesangbin@outlook.com>
2022-03-26 10:58:51 +0100

    add reset css style

--- src/theme/BinuiThemeProvider.tsx
+++ src/theme/BinuiThemeProvider.tsx
@@ -1,14 +1,17 @@
 import { PropsWithChildren } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
 import { BinuiTheme } from './BinuiTheme';
 
 interface BinuiThemeProviderProps {
     theme: BinuiTheme;
 }
 
+
 const BinuiThemeProvider = ({ theme, children }: PropsWithChildren<BinuiThemeProviderProps>) => {
     return (
         <ThemeProvider theme={theme}>
+            <Reset/>
             {children}
         </ThemeProvider>
     );
