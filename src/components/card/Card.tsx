4aa19ac51c6d1d6a4391b2c26b24126bb6afe78e
Sangbin Lee <leesangbin@outlook.com>
2022-03-27 22:27:48 +0200

    change Card's shadow

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -6,9 +6,15 @@ import { BinuiTheme } from '../../theme';
 const StyledCard = styled.div(({ theme }: { theme: BinuiTheme }) => css`
   border-radius: ${theme.borderRadius};
   background-color: ${theme.colors.paper[theme.mode]};
-  box-shadow: ${`3px 3px 10px 0px ${darken(0.15, theme.colors.background[theme.mode])}`};
+  box-shadow: ${`1px 1px 5px 0px ${darken(0.15, theme.colors.background[theme.mode])}`};
+
   padding: 8px;
   transition: all 0.25s;
+
+  &:hover {
+    box-shadow: ${`3px 3px 8px 0px ${darken(0.15, theme.colors.background[theme.mode])}`};
+
+  }
 `);
 
 const Card = ({ ...divProps }: HTMLAttributes<HTMLDivElement>) => {
