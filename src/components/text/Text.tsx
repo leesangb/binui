a56b2091c7c453e61b5397aaf8088839441a96b2
Sangbin Lee <leesangbin@outlook.com>
2022-03-28 21:40:53 +0200

    add fontsize in text

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -19,30 +19,39 @@ type TextProps<T extends TextComponentType | undefined> = BinuiTextProps<T> & JS
 const styleMapping: Record<TextComponentType, (theme: BinuiTheme) => FlattenSimpleInterpolation> = {
     h1: theme => css`
       font-weight: 700;
+      font-size: 2em;
     `,
     h2: theme => css`
       font-weight: 700;
+      font-size: 1.8em;
     `,
     h3: theme => css`
       font-weight: 600;
+      font-size: 1.6em;
     `,
     h4: theme => css`
       font-weight: 600;
+      font-size: 1.4em;
     `,
     h5: theme => css`
       font-weight: 500;
+      font-size: 1.2em;
     `,
     h6: theme => css`
       font-weight: 500;
+      font-size: 1em;
     `,
     div: theme => css`
       font-weight: 400;
+      font-size: 1em;
     `,
     span: theme => css`
       font-weight: 400;
+      font-size: 1em;
     `,
     p: theme => css`
       font-weight: 400;
+      font-size: 1em;
     `,
 };
 
