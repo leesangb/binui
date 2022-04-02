0df3c8e1fdb8d72c83d27d36cd83c1b2798a0598
Sangbin Lee <leesangbin@outlook.com>
2022-04-02 14:17:09 +0200

    change Card stories example

--- src/components/card/stories/Card.stories.tsx
+++ src/components/card/stories/Card.stories.tsx
@@ -1,5 +1,4 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import Text from '../../text/Text';
 import Card from '../Card';
 
 export default {
@@ -13,17 +12,17 @@ const Template: ComponentStory<typeof Card> = (args => <Card {...args}/>);
 export const Default = Template.bind({});
 Default.args = {
     variant: 'default',
-    children: <Text>크하하항</Text>,
+    children: '와우~ 친구들! 빡빡이 아저씨야',
 };
 
 export const Outlined = Template.bind({});
 Outlined.args = {
     variant: 'outlined',
-    children: <Text>크하하항</Text>,
+    children: '난 여기서 빠져나가야 되겠어',
 };
 
 export const Raised = Template.bind({});
 Raised.args = {
     variant: 'raised',
-    children: <Text>크하하항</Text>,
+    children: '붕 뜬 기분이다',
 };
